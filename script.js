3e663974c6d5fe65e237b3282fdbb96ebb60b840
unknown <paataugrekhelidze@gmail.com>
2021-02-28 21:57:48 -0500

    General Page replaced with Highcarts drilldown. News retrieved via Google Search API. Loader Page Added

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+document.querySelectorAll(".collection-item").forEach((item) => {
+  // listen to click event on the menu
+  item.addEventListener("click", (e) => {
+    let pageName;
+    // figure out which menu option was clicked
+    if (e.target.nodeName === "A") {
+      pageName = e.target.text;
+    } else if (e.target.nodeName === "LI") {
+      pageName = e.target.firstElementChild.children[1].text;
+    } else if (e.target.nodeName === "DIV") {
+      pageName = e.target.children[1].text;
+    } else {
+      pageName = e.target.nextSibling.text;
+    }
+
+    // loop through all content and only display div that corresponds to a clicke page name
+    Array.from(document.querySelector(".active-content").children).forEach(
+      (item) => {
+        if (item.id !== pageName) {
+          item.style.display = "none";
+        } else {
+          item.style.display = "block";
+        }
+      }
+    );
+  });
+});
+
+const populateNews = async (brand) => {
+  // fetch google search results
+  let API_KEY = "AIzaSyCRPkErSNfWGpJ0O_TyoHHSWNVjuUItugs";
+  let SEARCH_ENGINE = "b206e2519f1e09ec4";
+  let link = `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${SEARCH_ENGINE}&q=${brand}+sustainability&num=5`;
+  let data = await fetch(link, {
+    headers: { Accept: "application/json" },
+    method: "GET",
+  });
+  data = await data.json();
+
+  // loop through li elements and insert a element pointing to api search results for sustainability(topic might change)
+  Array.from(document.querySelector("#news-list").children).forEach(
+    (item, index) => {
+      let newlink = document.createElement("a");
+      newlink.textContent = data["items"][index]["title"];
+      newlink.setAttribute("href", data["items"][index]["link"]);
+      newlink.setAttribute("target", "_blank");
+      item.appendChild(newlink);
+    }
+  );
+};
+
 const wikiRate = async (link) => {
   let data = await fetch(link, {
     headers: { Accept: "application/json" },
@@ -6,8 +57,8 @@ const wikiRate = async (link) => {
   data = await data.json();
 
   let score = Number.parseFloat(data.value).toFixed(1);
-  console.log(data);
-  console.log(score);
+  // console.log(data);
+  // console.log(score);
   return parseFloat(score);
   // let src = document.getElementById(id);
   // src.textContent = score;
@@ -46,69 +97,8 @@ document.addEventListener("DOMContentLoaded", async function () {
       let src = document.getElementById("logo-container");
       src.appendChild(img);
 
-      /**
-       * 
-       // get and insert wikiRate score data
-       // insert total
-       await wikiRate(
-         "https://wikirate.org/Fashion_Revolution%2BFashion_Transparency_Index_2020+" +
-           brand +
-           "+2020",
-         "total"
-       );
- 
-       // insert policy/commitment score
-       await wikiRate(
-         "https://wikirate.org/Fashion_Revolution+1_Policy_Commitments_Score+" +
-           brand +
-           "+2020",
-         "commitment"
-       );
- 
-       // insert governance score
-       await wikiRate(
-         "https://wikirate.org/Fashion_Revolution+2_Governance_Score+" +
-           brand +
-           "+2020",
-         "governance"
-       );
- 
-       // insert traceability score
-       await wikiRate(
-         "https://wikirate.org/Fashion_Revolution+3_Traceability_Score+" +
-           brand +
-           "+2020",
-         "traceability"
-       );
- 
-       // insert ksf score
-       await wikiRate(
-         "https://wikirate.org/Fashion_Revolution+4_Know_Show_Fix_Score+" +
-           brand +
-           "+2020",
-         "ksf"
-       );
- 
-       // insert spotlight score
-       await wikiRate(
-         "https://wikirate.org/Fashion_Revolution+5_Spotlight_Issues_Score+" +
-           brand +
-           "+2020",
-         "spotlight"
-       );
-       */
-      // await wikiRate(
-      //   "https://wikirate.org/Fashion_Revolution%2BFashion_Transparency_Index_2020+" +
-      //     brand +
-      //     "+2020",
-      //   "total"
-      // );
-
-      // console.log(overall);
-      // console.log(typeof overall);
-
       // Create the chart
-      Highcharts.chart("container", {
+      Highcharts.chart("General", {
         chart: {
           type: "column",
         },
@@ -138,6 +128,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         plotOptions: {
           series: {
             borderWidth: 0,
+            shadow: false,
             dataLabels: {
               enabled: true,
               format: "{point.y:.1f}",
@@ -165,16 +156,6 @@ document.addEventListener("DOMContentLoaded", async function () {
                 ),
                 drilldown: "overall",
               },
-              {
-                name: "Firefox",
-                y: 10.57,
-                drilldown: "Firefox",
-              },
-              {
-                name: "Internet Explorer",
-                y: 7.23,
-                drilldown: "Internet Explorer",
-              },
             ],
           },
         ],
@@ -184,22 +165,24 @@ document.addEventListener("DOMContentLoaded", async function () {
               name: "overall",
               id: "overall",
               data: [
-                [
-                  "Policy & Commitments",
-                  await wikiRate(
+                {
+                  name: "Policy & Commitments",
+                  y: await wikiRate(
                     "https://wikirate.org/Fashion_Revolution+1_Policy_Commitments_Score+" +
                       brand +
                       "+2020"
                   ),
-                ],
-                [
-                  "Governance",
-                  await wikiRate(
+                  drilldown: "Policy & Commitments",
+                },
+                {
+                  name: "Governance",
+                  y: await wikiRate(
                     "https://wikirate.org/Fashion_Revolution+2_Governance_Score+" +
                       brand +
                       "+2020"
                   ),
-                ],
+                  drilldown: "Governance",
+                },
                 {
                   name: "Traceability",
                   y: await wikiRate(
@@ -210,129 +193,277 @@ document.addEventListener("DOMContentLoaded", async function () {
                   drilldown: "Traceability",
                 },
 
-                [
-                  "Know,show & Fix",
-                  await wikiRate(
+                {
+                  name: "Know,show & Fix",
+                  y: await wikiRate(
                     "https://wikirate.org/Fashion_Revolution+4_Know_Show_Fix_Score+" +
                       brand +
                       "+2020"
                   ),
-                ],
-                [
-                  "Spotlight Issues",
-                  await wikiRate(
+                  drilldown: "Know,show & Fix",
+                },
+                {
+                  name: "Spotlight Issues",
+                  y: await wikiRate(
                     "https://wikirate.org/Fashion_Revolution+5_Spotlight_Issues_Score+" +
                       brand +
                       "+2020"
                   ),
-                ],
+                  drilldown: "Spotlight Issues",
+                },
               ],
             },
             {
+              name: "Policy & Commitments",
+              id: "Policy & Commitments",
+              data: [
+                {
+                  name: "Own Operations Policies",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+1_1_Own_Operations_Policies+Fashion_Revolution_Research_Group+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Supply Chain Policies",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+1_2_Supply_Chain_Policies+" +
+                      brand +
+                      "+2020"
+                  ),
+                  drilldown: "Supply Chain Policies",
+                },
+                {
+                  name: "Management Procedures",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+1_3_Management_Procedures+Fashion_Revolution_Research_Group+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Identification of Impacts",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+1_4_Identification_of_Impacts+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Strategic Plan for Improving Impacts",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+1_5_Strategic_Plan_for_Improving_Impacts+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Verified Sustainability Report",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+1_6_Verified_Sustainability_Report+Fashion_Revolution_Research_Group+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+              ],
+            },
+            {
+              name: "Supply Chain Policies",
+              id: "Supply Chain Policies",
+              data: [
+                {
+                  name: "Supply Chain Policies in Local Language",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+Supply_Chain_Policies_in_Local_Language+Fashion_Revolution_Research_Group+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Supply Chain Policies Are Contractual",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+Supply_Chain_Policies_Are_Contractual+Fashion_Revolution_Research_Group+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Supply Chain Policies",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+Supply_Chain_Policies+Fashion_Revolution_Research_Group+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name:
+                    "Supply Chain Policies Align with International Standards",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+Supply_Chain_Policies_Align_with_International_Standards+Fashion_Revolution_Research_Group+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+              ],
+            },
+            {
+              name: "Governance",
+              id: "Governance",
+              data: [
+                {
+                  name:
+                    "Identifies Lead Responsibility for Human Rights & Environmental Issues",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+2_1_Identifies_Lead_Responsibility_for_Human_Rights_Environmental_Issues+Fashion_Revolution_Research_Group+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name:
+                    "Board Accountability for Human Rights & Environmental Issues",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+2_2_Board_Accountability_for_Human_Rights_Environmental_Issues+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name:
+                    "Incorporation of Human Rights & Environmental Performance in Purchasing Practices",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+2_3_Incorporation_of_Human_Rights_Environmental_Performance_in_Purchasing_Practices+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+              ],
+            },
+            {
+              name: "Traceability",
               id: "Traceability",
               data: [
-                [
-                  "Tier One Factory Disclosure",
-                  await wikiRate(
+                {
+                  name: "Tier One Factory Disclosure",
+                  y: await wikiRate(
                     "https://wikirate.org/Fashion_Revolution+3_1_Tier_One_Factory_Disclosure+Fashion_Revolution_Research_Group+" +
                       brand +
                       "+2020"
                   ),
-                ],
-                [
-                  "Processing Facilities Disclosure",
-                  await wikiRate(
+                },
+                {
+                  name: "Processing Facilities Disclosure",
+                  y: await wikiRate(
                     "https://wikirate.org/Fashion_Revolution+3_2_Processing_Facilities_Disclosure+Fashion_Revolution_Research_Group+" +
                       brand +
                       "+2020"
                   ),
-                ],
-                [
-                  "Raw Materials Suppliers Disclosure",
-                  await wikiRate(
+                },
+                {
+                  name: "Raw Materials Suppliers Disclosure",
+                  y: await wikiRate(
                     "https://wikirate.org/Fashion_Revolution+3_3_Raw_Materials_Suppliers_Disclosure+Fashion_Revolution_Research_Group+" +
                       brand +
                       "+2020"
                   ),
-                ],
+                },
               ],
             },
             {
-              name: "Firefox",
-              id: "Firefox",
+              name: "Know,show & Fix",
+              id: "Know,show & Fix",
               data: [
-                ["v58.0", 1.02],
-                ["v57.0", 7.36],
-                ["v56.0", 0.35],
+                {
+                  name: "Know, Show & Fix: Due Diligence",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+4_1_Know_Show_Fix_Due_Diligence+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Know: Implementation of Supply Chain Policies",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+4_2_Know_Implementation_of_Supply_Chain_Policies+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Show: Facility Assessments",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+4_3_Show_Facility_Assessments+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Fix: Remediation",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+4_4_Fix_Remediation+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Fix: Grievance Management",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+4_5_Fix_Grievance_Management+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
               ],
             },
             {
-              name: "Internet Explorer",
-              id: "Internet Explorer",
+              name: "Spotlight Issues",
+              id: "Spotlight Issues",
               data: [
-                ["v11.0", 6.2],
-                ["v10.0", 0.29],
-                ["v9.0", 0.27],
-                ["v8.0", 0.47],
+                {
+                  name: "Conditions",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+5_1_Conditions+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Consumption",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+5_2_Consumption+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Composition",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+5_3_Composition+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
+                {
+                  name: "Climate",
+                  y: await wikiRate(
+                    "https://wikirate.org/Fashion_Revolution+5_4_Climate+" +
+                      brand +
+                      "+2020"
+                  ),
+                },
               ],
             },
           ],
         },
       });
 
-      //   var chart = new Highcharts.Chart({
-      //     chart: {
-      //         renderTo: 'container',
-      //         type: 'pie'
-      //     },
-      //     series: [{
-      //         data: myInitialDataArray, // make sure each data point has an id
-      //         point: {
-      //             events: {
-      //                 click: function () {
-      //                     $.post('/get/data/by/id/' + this.id, function(data) {
-      //                         // you may need to format your data here
-      //                         chart.series[0].setData(data);
-      //                     });
-      //                 }
-      //             }
-      //         }
-      //     }]
-      // });
+      // build news list
+      populateNews(brand);
 
-      /**
-       // load up score circle
-       // Find all rating items
-       const ratings = document.querySelectorAll(".rating");
- 
-       // Iterate over all rating items
-       ratings.forEach((rating) => {
-         // Get content and get score as an int
-         const ratingContent = rating.innerHTML;
-         const ratingScore = parseInt(ratingContent, 10);
- 
-         // Define if the score is good, meh or bad according to its value
-         const scoreClass =
-           ratingScore < 4 ? "bad" : ratingScore < 6 ? "meh" : "good";
- 
-         // Add score class to the rating
-         rating.classList.add(scoreClass);
- 
-         // After adding the class, get its color
-         const ratingColor = window.getComputedStyle(rating).backgroundColor;
- 
-         // Define the background gradient according to the score and color
-         const gradient = `background: conic-gradient(${ratingColor} ${
-           ratingScore * 10
-         }%, transparent 0 100%)`;
- 
-         // Set the gradient as the rating background
-         rating.setAttribute("style", gradient);
- 
-         // Wrap the content in a tag to show it above the pseudo element that masks the bar
-         rating.innerHTML = `<span>${ratingScore}</span>`;
-       });
-       */
+      // turn off loader
+      document.querySelector("#loader").style.display = "none";
+      document.querySelector("#General").style.display = "block";
     }
   );
 });
